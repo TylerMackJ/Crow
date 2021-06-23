0bb332d97de80e9b66206a0105e6f5088f2e3bfb
Tyler Johnson <tylermackj@gmail.com>
2021-06-23 16:20:31 -0500

    Added enviorment variable test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::env;
+
+struct Flags {
+    text: Option<String>,
+    photo: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    //parse_args(env::args().collect());
+
+    match env::var("API") {
+        Ok(val) => println!("{:?}", val),
+        Err(e) => println!("{}", e),
+    }
+}
+
+fn parse_args(mut args: Vec<String>) -> Flags {
+    let mut ret: Flags = Flags {
+        text: None,
+        photo: None,
+    };
+
+    // Remove binary 
+    args.remove(0);
+
+    // Remove and parse options
+    while args.len() > 0 {
+        match args.remove(0) {
+            // Handle -p
+            p if args.len() > 0 && p == "-p" => ret.text = Some(args.remove(0)),
+            // Handle text
+            text if ret.text == None => ret.text = Some(text),
+            // Panic
+            a => panic!("Unhandled Argument: {}", a),
+        }
+    }
+
+    ret
 }
